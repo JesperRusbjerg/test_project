e87c12ae437f6504e9a2b005b0a30de5b01a966e
Jesper <jru@cvation.com>
2023-02-21 15:11:07 +0100

    lala

--- tp_src/api/api.py
+++ tp_src/api/api.py
@@ -1,4 +1,4 @@
-from tp_src.tp_core.core import add
+# from tp_src.tp_core.core import add
 
 from tp_src.api.sub_api import sub_api
 
@@ -7,4 +7,4 @@ from tp_src.api.sub_api import sub_api
 
 
 def get(x, y):
-    return add(x, y)
+    print("lollers")
--- tp_src/tp_core/sub_core/sub_core_file.py
+++ tp_src/tp_core/sub_core/sub_core_file.py
@@ -4,6 +4,7 @@ from tp_src.tp_core.core import add
 
 from tp_src.api.sub_api import sub_api
 
+
 def return_meaning_of_life():
-    add(2,2)
+    add(2, 2)
     return 42
